9079ee22f402389eafb1c09331220a3562b761dd
GPallavicini <207866552+MartinaDamato@users.noreply.github.com>
2025-10-06 10:07:10 -0300

    commit subo tablas
    Hicimos pedido GET y POST de las tablas

--- back/index.js
+++ back/index.js
@@ -44,6 +44,124 @@ app.get('/', function (req, res) {
     });
 });
 
+app.get('/players', async function (req, res) {
+    let respuesta;
+    if (req.query.id_user != undefined) {
+        respuesta = await realizarQuery(`SELECT * FROM Players WHERE id_player=${req.query.id_player}`)
+    } else {
+        respuesta = await realizarQuery("SELECT * FROM Players");
+    }
+    res.send(respuesta);
+});
+
+app.get('/games', async function (req, res) {
+    let respuesta;
+    if (req.query.id_user != undefined) {
+        respuesta = await realizarQuery(`SELECT * FROM Games WHERE id_game=${req.query.id_game}`)
+    } else {
+        respuesta = await realizarQuery("SELECT * FROM Games");
+    }
+    res.send(respuesta);
+});
+
+app.get('/customers', async function (req, res) {
+    let respuesta;
+    if (req.query.id_user != undefined) {
+        respuesta = await realizarQuery(`SELECT * FROM Customers WHERE id_customer=${req.query.id_customer}`)
+    } else {
+        respuesta = await realizarQuery("SELECT * FROM Customers");
+    }
+    res.send(respuesta);
+});
+
+app.get('/playersxgame', async function (req, res) {
+    let respuesta;
+    if (req.query.id_user != undefined) {
+        respuesta = await realizarQuery(`SELECT * FROM PlayersxGame WHERE id_playerxgame=${req.query.id_playerxgame}`)
+    } else {
+        respuesta = await realizarQuery("SELECT * FROM PlayersxGame");
+    }
+    res.send(respuesta);
+});
+
+app.get('/results', async function (req, res) {
+    let respuesta;
+    if (req.query.id_user != undefined) {
+        respuesta = await realizarQuery(`SELECT * FROM Results WHERE id_result=${req.query.id_result}`)
+    } else {
+        respuesta = await realizarQuery("SELECT * FROM Results");
+    }
+    res.send(respuesta);
+});
+
+app.post('/players', async function (req, res) {
+    console.log(req.body);
+    try {
+        if (req.body.avatar == undefined || req.body.avatar == "") {
+            req.body.avatar = null;
+        }
+        const player = await realizarQuery(`
+            INSERT INTO Players (username, email, password, avatar) VALUES
+            ('${req.body.username}', '${req.body.email}', '${req.body.password}', '${req.body.avatar}');
+        `);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.post('/customers', async function (req, res) {
+    console.log(req.body);
+    try {
+        if (req.body.avatar_costumer == undefined || req.body.avatar_costumer == "") {
+            req.body.avatar_costumer = null;
+        }
+        const customer = await realizarQuery(`
+            INSERT INTO Customers (name, avatar_costumer, day) VALUES
+            ('${req.body.name}', '${req.body.avatar_costumer}', '${req.body.day}');
+        `);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
+app.post('/games', async function (req, res) {
+    console.log(req.body);
+    try {
+        const game = await realizarQuery(`
+            INSERT INTO Games (day) VALUES
+            ('${req.body.day}');
+        `);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.post('/playersxgame', async function (req, res) {
+    console.log(req.body);
+    try {
+        const playerxgame = await realizarQuery(`
+            INSERT INTO PlayersxGame (id_player, id_game) VALUES
+            ('${req.body.id_player}', '${req.body.id_game}');
+        `);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.post('/results', async function (req, res) {
+    console.log(req.body);
+    try {
+        const result = await realizarQuery(`
+            INSERT INTO Results (id_game, id_player, money, points, time ) VALUES
+            ('${req.body.id_game}', '${req.body.id_player}', '${req.body.money}', '${req.body.points}', '${req.body.time}');
+        `);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
 app.post('/loginUser', async function (req, res) {
     console.log("Resultado de búsqueda:", req.body);
     try {
