9584de2e1888efc59376375133b0fc71a5f90a46
smartin26 <smartin@pioix.edu.ar>
2025-11-13 08:48:01 -0300

    commit socket

--- back/index.js
+++ back/index.js
@@ -23,8 +23,7 @@ const io = require('socket.io')(server, {
         origin: [
             "http://localhost:3000", 
             "http://localhost:3001",
-            "http://192.168.0.8:3000",
-            "http://192.168.0.175:3000"
+            "http://10.1.4.24:3000"
         ],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
